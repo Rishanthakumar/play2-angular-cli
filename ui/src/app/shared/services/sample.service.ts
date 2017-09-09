095569e83d0cbc1459b710ae4cfc9c66bcee2a38
Rishanthakumar <rishanthakumar@gmail.com>
2017-09-09 18:29:43 +0530

    Change sample service response._body() to response.text() and code refactor.

--- ui/src/app/shared/services/sample.service.ts
+++ ui/src/app/shared/services/sample.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, RequestOptions } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { URL_CONST } from '../config/url.constants';
 import 'rxjs/Rx';
 
 @Injectable()
 export class SampleService {
 
-  options: RequestOptions;
-
   constructor(private http: Http) {}
 
   getHelloWorld() {
     return this.http.get(URL_CONST.URL_PREFIX + 'api/v1/hello-world')
-      .map((response: Response) => response._body);
+      .map((response: Response) => response.text());
   }
 
 }
