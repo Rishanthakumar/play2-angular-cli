a141bcce732f1607dfae39438c775c419e0ab168
Saad Sahibjan <saad.sahibjan@gmail.com>
2017-09-09 17:55:59 +0530

    Change response message of hello world service

--- app/controllers/ServiceController.java
+++ app/controllers/ServiceController.java
@@ -5,7 +5,7 @@ import play.mvc.Result;
 public class ServiceController extends BaseController {
 
     public Result helloWorldService() {
-        return ok("Hello World");
+        return ok("Hello World - I came through Play2 Service");
     }
 
 }
